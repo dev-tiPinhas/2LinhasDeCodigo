b744020e7aafe9485edfdd9cce2ef0348efdea3a
Tiago Pinheiro <tiago.pinheiro@itsector.pt>
2020-10-07 15:27:39 +0100

    Initial Commit

---
+++ 2linhasdecodigo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  2linhasdecodigo
+//
+//  Created by Tiago Pinheiro on 07/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2linhasdecodigo/_linhasdecodigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _linhasdecodigoApp.swift
+//  2linhasdecodigo
+//
+//  Created by Tiago Pinheiro on 07/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct _linhasdecodigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
